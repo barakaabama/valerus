50fd25a364bafe115ae3c369586693801a675480
barakaabama <109741981+barakaabama@users.noreply.github.com>
2022-07-21 16:23:59 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+URL_VID = os.getenv("URL_VID")
+URL_PROC = os.getenv("URL_PROC")
+URL_BLOCK = os.getenv("URL_BLOCK")
+URL_MAT = os.getenv("URL_MAT")
